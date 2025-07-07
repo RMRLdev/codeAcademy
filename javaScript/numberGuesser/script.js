31b0c42f9c3a8dc87f2c3aac3f250233da789837
Riley <mrrlarg@gmail.com>
2025-07-07 11:44:33 +0100

    second function

--- javaScript/numberGuesser/script.js
+++ javaScript/numberGuesser/script.js
@@ -7,3 +7,13 @@ let currentRoundNumber = 1;
 function generateTarget(){
     return Math.floor(Math.random()*9);
 }
+function compareGuesses(human,computer,secret){
+    /*checks if the difference between the huamn guess 
+    and the secret number is smaller than the difference 
+    between the computer guess and the secret number
+    */
+    if((human-secret)<(computer-secret)){
+        return true;
+    }else {return false;}
+}
+console.log(compareGuesses(1,2,3));
