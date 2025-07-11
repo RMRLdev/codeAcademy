6ceb60ee676c9413c98c8cac7f06cbe0cb8ec52d
Riley <mrrlarg@gmail.com>
2025-07-11 13:53:43 +0100

    added event listeners

--- porfolioWebsite/resources/indexJS.js
+++ porfolioWebsite/resources/indexJS.js
@@ -1,1 +1,11 @@
 console.log("testing");
+let logoElements=document.getElementsByClassName('logo');
+function growLogo(){
+    console.log("Q");
+}
+document.getElementById('skills').addEventListener("click", growLogo);
+//logo.addEventListener('mouseover',growLogo);
+console.log(logoElements.length);
+for(let i=0;i<logoElements.length;i++){
+    logoElements[i].addEventListener('mouseover',growLogo);
+};
