719df1d81a00f9d257db80808008723b2c46bbcf
Riley <mrrlarg@gmail.com>
2025-07-11 14:31:32 +0100

    added grow and shrink functions js v1

--- porfolioWebsite/resources/indexJS.js
+++ porfolioWebsite/resources/indexJS.js
@@ -1,11 +1,26 @@
-console.log("testing");
-let logoElements=document.getElementsByClassName('logo');
-function growLogo(){
-    console.log("Q");
+//console.log("testing");
+let logoElements=document.getElementById('skills').getElementsByClassName('logo');
+function growLogo(event){
+    //console.log("Q");
+    event.currentTarget.querySelector('img').style.width='53px';
+    event.currentTarget.querySelector('img').style.height='auto';
+    let focus=event.currentTarget.querySelector("p")
+    focus.style.display='block';
+    event.currentTarget.style.display='flex';
+    event.currentTarget.querySelector('p').style.paddingLeft='5px';
 }
+function shrinkLogo(event){
+    event.currentTarget.querySelector('img').style.width='50px';
+    event.currentTarget.querySelector('img').style.height='auto';
+    event.currentTarget.querySelector('p').style.display='none';
+    event.currentTarget.style.display='list-item';
+    event.currentTarget.querySelector('p').style.paddingLeft='0';
+}
+//#skills > ul > li:nth-child(2) > p
 document.getElementById('skills').addEventListener("click", growLogo);
 //logo.addEventListener('mouseover',growLogo);
 console.log(logoElements.length);
 for(let i=0;i<logoElements.length;i++){
     logoElements[i].addEventListener('mouseover',growLogo);
+    logoElements[i].addEventListener('mouseout',shrinkLogo);
 };
